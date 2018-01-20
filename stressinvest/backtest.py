d7102f8e467266974730d3a9877b11d670d612a8
vl-lemos <vl-lemos@uol.com.br>
2018-01-20 14:20:30 -0200

    implementation

--- stressinvest/backtest.py
+++ stressinvest/backtest.py
@@ -4,6 +4,7 @@ import numpy as np
 import sqlite3
 import datetime
 import talib
+import math
 
 from strategy import strategy_ema
 from aux_functions import convert_candle
@@ -37,13 +38,14 @@ aux_str+=table+';'
 cursor.execute(aux_str)
 
 candles=np.array(cursor.fetchall()) #id, start, open, high, low, close, , vwp, volume, trades
-candles=candles[:, [1,2,3,4,5,7]]# simplify to timestamp, open, high, low, close, volume
-
+#candles=candles[:, [1,2,3,4,5,7]]# simplify to timestamp, open, high, low, close, volume
+candles=candles[:, [1,4,2,5,3,7]]# simplify to timestamp, low, open, close, high, volume
 
 # Pass Database to strategy - will return buy/sell points
 outperiod=30*60 #seconds
 
 decision_table=strategy_ema(candles, outperiod)
+
 # Backtest - Calculate transactions
 
 
@@ -53,7 +55,13 @@ decision_table=strategy_ema(candles, outperiod)
 outcandles=convert_candle(candles,30*60)
 
 # Calculate Indicators - EMA, DMI, Stoch
+short_ema=talib.EMA(outcandles[:,3], timeperiod=9)
+long_ema=talib.EMA(outcandles[:,3], timeperiod=20)
 
+plus_di=talib.PLUS_DI(outcandles[:,4], outcandles[:,1], outcandles[:,3], timeperiod=14) # PLUS_DI(high, low, close, timeperiod=14)
+minus_di=talib.MINUS_DI(outcandles[:,4], outcandles[:,1], outcandles[:,3], timeperiod=14) #MINUS_DI(high, low, close, timeperiod=14)
+
+slowk, slowd = talib.STOCH(outcandles[:,4], outcandles[:,1], outcandles[:,3], fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
 # Write Graph
 
 # Write Backtest Results - Buy N HOld, profit for each transaction 
--- stressinvest/strategy.py
+++ stressinvest/strategy.py
@@ -1,6 +1,6 @@
 from aux_functions import convert_candle
 import talib
-
+import math
 
 def strategy_ema(candles, outperiod):
     # Define the decision period - in minutes
@@ -9,6 +9,30 @@ def strategy_ema(candles, outperiod):
     # Convert candles to decision period
     outcandles=convert_candle(candles,decision_period*60)
     
+    # Calculate indicators - EMA, DMI
+    short_ema=talib.EMA(outcandles[:,3], timeperiod=9)
+    long_ema=talib.EMA(outcandles[:,3], timeperiod=20)
+    ema_diff=short_ema-long_ema
     
-    # Calculate indicators - EMA, DMI, 
+    plus_di=talib.PLUS_DI(outcandles[:,4], outcandles[:,1], outcandles[:,3], timeperiod=14) # PLUS_DI(high, low, close, timeperiod=14)
+    minus_di=talib.MINUS_DI(outcandles[:,4], outcandles[:,1], outcandles[:,3], timeperiod=14) #MINUS_DI(high, low, close, timeperiod=14)
+    dm_diff=plus_di-minus_di
     
+    # Iterate over the candles and determine buy/sell
+    decision_table=[]
+    for i in range(len(outcandles)):
+        if math.isnan(ema_diff[i]) or math.isnan(dm_diff[i]):
+            decision_table.append("") # do nothing
+        else:
+            decision_table.append("")
+            if ema_diff[i]>0 and ema_diff[i-1]<0: # if crossed from below, buys
+                if dm_diff[i]>0: # confirm with directional movement
+                    decision_table[i]="buy"
+            elif ema_diff[i]<0 and ema_diff[i-1]>0: #if cross from above, sells
+                if dm_diff[i]<0: # confirm with directional movement
+                    decision_table[i]="sell"
+                
+        # Check if it has crossed the ema_diff
+        
+    
+    return decision_table
