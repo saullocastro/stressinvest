2359cfd6c965cd7298fff4f3a2989e955a87ee9a
vl-lemos <vl-lemos@uol.com.br>
2018-01-20 14:58:54 -0200

    backtest

--- stressinvest/backtest_calc.py
+++ stressinvest/backtest_calc.py
@@ -9,8 +9,16 @@ def backtest_calc(candles, outcandles, decision_table):
     for i in range(len(decision_table)):
         if decision_table[i]=="buy":
             if currency_1>0: # if it is not bought, buys
-                price=outcandles[i]
-                currency_2=currency_1*
-                transaction.add(["buy",])
-    
+                price=outcandles[i,3] # close price
+                currency_2=currency_1/price*(1-trader_fee/100) # applies fees
+                fee=currency_1*(trader_fee/100)
+                currency_1=0
+                transaction.append(["buy",currency_1, currency_2, fee])
+        if decision_table[i]=="sell":
+            if currency_2>0: # if it is not sold, sells
+                price=outcandles[i,3] # close price
+                currency_1=currency_2/price*(1-trader_fee/100) # applies fees
+                currency_2=0
+                fee=currency_2*price*(trader_fee/100)
+                transaction.append(["buy",currency_1, currency_2, fee])    
     # Ends sold
