d402cf4f25b455aca55b8f688ccf1921bbd4f835
saullocastro <saullogiovani@gmail.com>
2018-02-22 18:57:44 -0300

    ENH: using TradeData class

--- stressinvest/strategy.py
+++ stressinvest/strategy.py
@@ -3,24 +3,24 @@ import talib
 #import math
 #import numpy as np
 
-def strategy(candles, market_status, strategy_type, args):
-    if strategy_type=="ema":
-        decision=strategy_ema(candles, market_status, args[0])
+def strategy(candles, strategy_type, tradedata):
+    if strategy_type == "ema":
+        decision = strategy_ema(candles, tradedata)
     return decision
 
-def strategy_ema(candles, market_status, outperiod): 
+def strategy_ema(candles, tradedata):
     # Convert candles to period
-    outcandles=convert_candle_db(candles,outperiod)
-    
+    outcandles = convert_candle_db(candles, tradedata.candle_size)
+
     # Calculate indicators - EMA, DMI
-    short_ema=talib.EMA(outcandles[:,5], timeperiod=9)
-    long_ema=talib.EMA(outcandles[:,5], timeperiod=20)
-    ema_diff=short_ema-long_ema
-    
+    short_ema = talib.EMA(outcandles[:, 5], timeperiod=9)
+    long_ema = talib.EMA(outcandles[:, 5], timeperiod=20)
+    ema_diff = short_ema - long_ema
+
     # Analyze indicator and take a decision
-    decision=""
-    if ema_diff[-1]>0 and ema_diff[-2]<0 and market_status=="sold": # if crossed from below, buys
-        decision="buy"
-    elif ema_diff[-1]<0 and ema_diff[-2]>0 and market_status=="bought": #if cross from above, sells
-        decision="sell" 
+    decision = ""
+    if ema_diff[-1] > 0 and ema_diff[-2] < 0 and tradedata.market_status == "sold": # if crossed from below, buys
+        decision = "buy"
+    elif ema_diff[-1] < 0 and ema_diff[-2] > 0 and tradedata.market_status == "bought": #if cross from above, sells
+        decision = "sell"
     return decision
--- stressinvest/trader.py
+++ stressinvest/trader.py
@@ -1,252 +1,270 @@
+import os
+import sys
+import time
+import getpass
+import math
+import sqlite3
+import datetime
+from functools import partial
+
+import numpy as np
 from binance.client import Client
 from binance.websockets import BinanceSocketManager
-from twisted.internet import reactor
+
+sys.path.append('.')
 from strategy import strategy
-import getpass
-import time
-import sqlite3
-import math
-import numpy as np
-import datetime
+
+
+class TradeData(object):
+    def __init__(self):
+        self.b_realtrade = True
+        self.b_warmedup = False
+        self.warmupperiod = 120*60*60 #60 hours - in seconds - depends on strategy
+        self.b_trade = False
+        self.market_status = None
+        self.exchange = "BINANCE" # To be Implemented Additional Exchanges
+        self.currency = "BTCUSDT"
+        self.asset1 = "BTC"
+        self.asset2 = "USDT"
+        self.precisionasset1 = 6 #depends on the asset
+        self.precisionasset2 = 6 #depends on the asset
+        self.trader_fee = 0.05 # in % obs, changes over time, how to get automatically?
+        self.db_name = "binance.db"
+        self.db_table = "candles_USDT_BTC"
+        self.candle_size = 1*60*60
 
 #------------------------------------------------------------------------------
-def ticker(msg):
-    # global variables
-    global b_warmedup
-    global b_trade
-    global market_status
-    global db_table
-    global db_name
-    
+def ticker(msg, td):
     # ticker
     #print(msg)
     #id, start, open, high, low, close, , vwp, volume, trades
-    s_start=msg['k']['t']/1000 #s
-    s_open=msg['k']['o']
-    s_high=msg['k']['h']
-    s_low=msg['k']['l']
-    s_close=msg['k']['c']
-    s_vwp=msg['k']['c'] #average weighted price - will assume close price
-    s_volume=msg['k']['v']
-    s_trades=msg['k']['t']
-    final_bar=msg['k']['x'] # if it is the final bar, insert in the database
+    s_start = msg['k']['t']/1000 #s
+    s_open = msg['k']['o']
+    s_high = msg['k']['h']
+    s_low = msg['k']['l']
+    s_close = msg['k']['c']
+    s_vwp = msg['k']['c'] #average weighted price - will assume close price
+    s_volume = msg['k']['v']
+    s_trades = msg['k']['t']
+    final_bar = msg['k']['x'] # if it is the final bar, insert in the database
     if final_bar: # candle has formed
-        db = sqlite3.connect(db_name)
+        db = sqlite3.connect(td.db_name)
         cursor = db.cursor()
-        cmd="INSERT INTO candles_USDT_BTC (start, open, high, low, close, vwp, volume, trades) VALUES ("
-        cmd+=str(s_start)
-        cmd+=","+str(s_open)
-        cmd+=","+str(s_high)
-        cmd+=","+str(s_low)
-        cmd+=","+str(s_close)
-        cmd+=","+str(s_vwp)
-        cmd+=","+str(s_volume)
-        cmd+=","+str(s_trades)
-        cmd+=");"
+        cmd = "INSERT INTO candles_USDT_BTC (start, open, high, low, close, vwp, volume, trades) VALUES ("
+        cmd += str(s_start)
+        cmd += "," + str(s_open)
+        cmd += "," + str(s_high)
+        cmd += "," + str(s_low)
+        cmd += "," + str(s_close)
+        cmd += "," + str(s_vwp)
+        cmd += "," + str(s_volume)
+        cmd += "," + str(s_trades)
+        cmd += ");"
         cursor.execute(cmd)
         db.commit()
         db.close()
     # call strategy
-    if b_warmedup:
-        db = sqlite3.connect(db_name)
+    if td.b_warmedup:
+        db = sqlite3.connect(td.db_name)
         cursor = db.cursor()
-        
+
         t1=time.time()
         t1-=t1%60-60 # last full minute candle
         t0=t1-120*60*60 #maximum number of data used - 120hr
-        
-        aux_str='SELECT * FROM '
-        aux_str+=db_table
-        aux_str+=' WHERE start BETWEEN %s AND %s' %(str(t0), str(t1))
-        
+
+        aux_str = 'SELECT * FROM '
+        aux_str += td.db_table
+        aux_str += ' WHERE start BETWEEN %s AND %s' %(str(t0), str(t1))
+
         cursor.execute(aux_str)
-        db_candles=cursor.fetchall()
+        db_candles = cursor.fetchall()
         db_candles.sort(key=lambda tup: tup[1]) # Order the results by timestamp
-        db_candles=np.array(db_candles)
-        decision=strategy(db_candles, market_status, "ema", [1*60*60])
+        db_candles = np.array(db_candles)
+        decision = strategy(db_candles, "ema", td)
         # call trade
         try:
             trade(decision)
         except:
             pass
 #------------------------------------------------------------------------------
-def warmup(exchange, currency, warmupperiod):
-    global b_warmedup
-    global db_table
-    global db_name
-    t1=time.time()
-    t1-=t1%60-60 # last full minute candle
-    t0=t1-warmupperiod
-    # Import DB results
+def warmup(td):
+    t1 = time.time()
+    t1 -= t1 % 60-60 # last full minute candle
+    t0 = t1-td.warmupperiod
 
-    db = sqlite3.connect(db_name)
+    if not os.path.isfile(td.db_name):
+        print('Creating database...')
+        db = sqlite3.connect(td.db_name)
+        cursor = db.cursor()
+        #id, start, open, high, low, close, , vwp, volume, trades
+        cmd = ("""CREATE TABLE %s (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            start INTEGER,
+            open REAL NOT NULL,
+            high REAL NOT NULL,
+            low REAL NOT NULL,
+            close REAL NOT NULL,
+            vwp REAL NOT NULL,
+            volume REAL NOT NULL,
+            trades INTEGER NOT NULL
+            )""" % td.db_table)
+        cursor.execute(cmd)
+        db.commit()
+        db.close()
+
+    # Import DB results
+    db = sqlite3.connect(td.db_name)
     cursor = db.cursor()
-    aux_str='SELECT * FROM '
-    aux_str+=db_table
-    aux_str+=' WHERE start BETWEEN %s AND %s' %(str(t0), str(t1))
-    
+    aux_str = 'SELECT * FROM '
+    aux_str += td.db_table
+    aux_str += ' WHERE start BETWEEN %s AND %s' %(str(t0), str(t1))
+
     cursor.execute(aux_str)
-    db_candles=cursor.fetchall()
+    db_candles = cursor.fetchall()
     db_candles.sort(key=lambda tup: tup[1]) # Order the results by timestamp
-    db_candles=np.array(db_candles)
-    #print(db_candles)
-    
+    db_candles = np.array(db_candles)
+
     # Scan the imported results to find gaps
-    v_gaps=[]
+    v_gaps = []
+    candle = None
     for i, candle in enumerate(db_candles):
-        if i==0: # check first candle
-            if candle[1]-t0>60: # there is a gap
-                v_gaps.append([t0,candle[1]])        
-        if i>0: # check the others
-            if candle[1]-db_candles[i-1][1]>60: # there is a gap
-                v_gaps.append([candle[1],db_candles[i-1][1]])
-    # check last candle
-    if t1-candle[1]>60: # there is a gap
-        v_gaps.append([candle[1],t1])   
-    
-    v_candles=[]
+        if i == 0: # check first candle
+            if candle[1]-t0 > 60: # there is a gap
+                v_gaps.append([t0, candle[1]])
+        if i > 0: # check the others
+            if candle[1]-db_candles[i-1][1] > 60: # there is a gap
+                v_gaps.append([candle[1], db_candles[i-1][1]])
+
+    if candle is not None:
+        # check last candle
+        if t1 - candle[1] > 60: # there is a gap
+            v_gaps.append([candle[1], t1])
+
+    v_candles = []
     for gap in v_gaps:
         # check if the gap is above 500 candles
-        startTime=gap[0]
-        endTime=gap[1]
-        v_aux=[]
+        startTime = gap[0]
+        endTime = gap[1]
+        v_aux = []
         if (endTime-startTime)/60>500:
-            aux_endTime=startTime
+            aux_endTime = startTime
             for i in range(int(math.floor((endTime-startTime)/60/500))):
-                aux_startTime=aux_endTime
-                aux_endTime=aux_startTime+500*60
-                if aux_endTime>endTime:
-                    aux_endTime=endTime
+                aux_startTime = aux_endTime
+                aux_endTime = aux_startTime+500*60
+                if aux_endTime > endTime:
+                    aux_endTime = endTime
                 v_aux.append([aux_startTime, aux_endTime])
         else:
             v_aux.append([startTime, endTime])
         # Get the candles
-        for aux in v_aux: 
-            startTime=int(aux[0])*1000 #binance uses ms
-            endTime=int(aux[1])*1000 #binance uses ms
-            candles = client.get_klines(symbol=currency, interval=Client.KLINE_INTERVAL_1MINUTE, limit=500, startTime=startTime, endTime=endTime)
+        for aux in v_aux:
+            startTime = int(aux[0])*1000 #binance uses ms
+            endTime = int(aux[1])*1000 #binance uses ms
+            candles = client.get_klines(symbol=td.currency, interval=Client.KLINE_INTERVAL_1MINUTE, limit=500, startTime=startTime, endTime=endTime)
             v_candles.append(candles)
-            
+
     # add candles to the database
     for list_candle in v_candles:
         for candle in list_candle:
-            s_start=candle[0]/1000 #s
-            s_open=candle[1]
-            s_high=candle[2]
-            s_low=candle[3]
-            s_close=candle[4]
-            s_vwp=candle[4] #average weighted price - will assume close price
-            s_volume=candle[5]
-            s_trades=candle[8]
-            cmd="INSERT INTO "+str(db_table)+" (start, open, high, low, close, vwp, volume, trades) VALUES ("
-            cmd+=str(s_start)
-            cmd+=","+str(s_open)
-            cmd+=","+str(s_high)
-            cmd+=","+str(s_low)
-            cmd+=","+str(s_close)
-            cmd+=","+str(s_vwp)
-            cmd+=","+str(s_volume)
-            cmd+=","+str(s_trades)
-            cmd+=");"
+            s_start = candle[0]/1000 #s
+            s_open = candle[1]
+            s_high = candle[2]
+            s_low = candle[3]
+            s_close = candle[4]
+            s_vwp = candle[4] #average weighted price - will assume close price
+            s_volume = candle[5]
+            s_trades = candle[8]
+            cmd = "INSERT INTO " + str(td.db_table)+" (start, open, high, low, close, vwp, volume, trades) VALUES ("
+            cmd += str(s_start)
+            cmd += ","+str(s_open)
+            cmd += ","+str(s_high)
+            cmd += ","+str(s_low)
+            cmd += ","+str(s_close)
+            cmd += ","+str(s_vwp)
+            cmd += ","+str(s_volume)
+            cmd += ","+str(s_trades)
+            cmd += ");"
             cursor.execute(cmd)
     db.commit()
     # Delete duplicate timestamp
-    cmd="DELETE FROM "+db_table+" WHERE rowid NOT IN (SELECT MAX(rowid) FROM "+db_table+" GROUP BY start);"
+    cmd = "DELETE FROM " + td.db_table + " WHERE rowid NOT IN (SELECT MAX(rowid) FROM " + td.db_table + " GROUP BY start);"
     cursor.execute(cmd)
     db.commit()
     db.close()
-    b_warmedup=True
+    td.b_warmedup = True
 #------------------------------------------------------------------------------
 def trade(decision):
-    global b_realtrade
-    global market_status
-    global asset1
-    global asset2
-    global precisionasset1
-    global precisionasset2
-    global currency
-    
-    if b_realtrade:
-        if decision=="buy":
+    if td.b_realtrade:
+        if decision == "buy":
             # get asset 2 balance to buy everything
-            balance = client.get_asset_balance(asset=asset2)
-            amt_str=math.floor(float(balance["free"]) * 10**precisionasset2) / 10**precisionasset2
-            amt_str = "{:0.0{}f}".format(amt_str, precisionasset2)
+            balance = client.get_asset_balance(asset=td.asset2)
+            amt_str = math.floor(float(balance["free"]) * 10**td.precisionasset2) / 10**td.precisionasset2
+            amt_str = "{:0.0{}f}".format(amt_str, td.precisionasset2)
             # read asset1 balance
-            balance_t0 = client.get_asset_balance(asset=asset1)
+            balance_t0 = client.get_asset_balance(asset=td.asset1)
             balance_t0 = float(balance_t0["free"])
-            order = client.order_market_buy(symbol=currency, quantity=amt_str)
+            order = client.order_market_buy(symbol=td.currency, quantity=amt_str)
             # read asset1 balance and calculate the price
-            balance_t1 = client.get_asset_balance(asset=asset1)
-            balance_t1=float(balance_t1["free"])
-            price=float(amt_str)/(balance_t1-balance_t0)/(1.-trader_fee/100.)  
-            fee=float(amt_str)*trader_fee/100.
-        if decision=="sell":
+            balance_t1 = client.get_asset_balance(asset=td.asset1)
+            balance_t1 = float(balance_t1["free"])
+            price = float(amt_str)/(balance_t1-balance_t0)/(1.-td.trader_fee/100.)
+            fee = float(amt_str)*td.trader_fee/100.
+        if decision == "sell":
             # get asset 1 balance to sell everything
-            balance = client.get_asset_balance(asset=asset1) 
-            amt_str=math.floor(float(balance["free"]) * 10**precisionasset1) / 10**precisionasset1
-            amt_str = "{:0.0{}f}".format(amt_str, precisionasset1) #amount to be sold
+            balance = client.get_asset_balance(asset=td.asset1)
+            amt_str = math.floor(float(balance["free"]) * 10**td.precisionasset1) / 10**td.precisionasset1
+            amt_str = "{:0.0{}f}".format(amt_str, td.precisionasset1) #amount to be sold
             # read asset2 balance to calculate the price
-            balance_t0 = client.get_asset_balance(asset=asset2)
+            balance_t0 = client.get_asset_balance(asset=td.asset2)
             balance_t0 = float(balance_t0["free"])
             # Market Order - TODO - EXCEPTION HANDLING if fails
-            order = client.order_market_sell(symbol=currency, quantity=amt_str) # if order["status"]=="FILLED" order is finished
+            order = client.order_market_sell(symbol=td.currency, quantity=amt_str) # if order["status"]=="FILLED" order is finished
             # read asset2 balance and calculate the price
-            balance_t1 = client.get_asset_balance(asset=asset2)
-            balance_t1=float(balance_t1["free"])
-            price=(balance_t1-balance_t0)/(1.-trader_fee/100.)/float(amt_str)
-            fee=amt_str*price*trader_fee/100. # fee always in asset2
+            balance_t1 = client.get_asset_balance(asset=td.asset2)
+            balance_t1 = float(balance_t1["free"])
+            price = (balance_t1-balance_t0)/(1.-td.trader_fee/100.)/float(amt_str)
+            fee = amt_str*price*td.trader_fee/100. # fee always in asset2
         # Log Trade
         #['Action','Balance Asset 1','Balance Asset 2','Paid Fee','Price','Date']
-        balance_1=client.get_asset_balance(asset=asset1)["free"]
-        balance_2=client.get_asset_balance(asset=asset2)["free"]
-        if decision!="":
+        balance_1 = client.get_asset_balance(asset=td.asset1)["free"]
+        balance_2 = client.get_asset_balance(asset=td.asset2)["free"]
+        if decision != "":
             with open("log.txt", 'a') as f:
-                aux=decision
-                aux+=", "+balance_1
-                aux+=", "+balance_2
-                aux+=", "+str(fee)
-                when=order["transactTime"]
-                when=datetime.datetime.fromtimestamp(float(when)).strftime('%m/%d/%Y %H:%M:%S')
+                aux = decision
+                aux += ", "+balance_1
+                aux += ", "+balance_2
+                aux += ", "+str(fee)
+                when = order["transactTime"]
+                when = datetime.datetime.fromtimestamp(float(when)).strftime('%m/%d/%Y %H:%M:%S')
                 f.write(aux)
     else:
         if decision!="":
             print("decision")
-    
+
 #------------------------------------------------------------------------------
 # Main Program
 # Get API Keys
-api_key=getpass.getpass(prompt="Insert API KEY: ")
-api_secret=getpass.getpass(prompt="Insert API SECRET: ")
+api_key = getpass.getpass(prompt="Insert API KEY: ")
+api_secret = getpass.getpass(prompt="Insert API SECRET: ")
 
-# Define exchance and currency
-exchange="BINANCE" # To be Implemented Additional Exchanges
-currency="BTCUSDT"
-asset1="BTC"
-asset2="USDT"
-precisionasset1=6 #depends on the asset
-precisionasset2=6 #depends on the asset
-trader_fee=0.05 # in % obs, changes over time, how to get automatically?
-db_name="binance.db"
-db_table="candles_USDT_BTC"
 
 # Start Client
 client = Client(api_key, api_secret)
 bm = BinanceSocketManager(client)
-conn_key = bm.start_kline_socket(currency, ticker, interval=client.KLINE_INTERVAL_1MINUTE)
+td = TradeData()
+
+ticker_partial = partial(ticker, td=td)
+conn_key = bm.start_kline_socket(td.currency, ticker_partial, interval=client.KLINE_INTERVAL_1MINUTE)
 bm.start() # Start ticker socket
 
 # Check Warmup Period
-b_warmedup=False
-warmupperiod= 120*60*60 #60 hours - in seconds - depends on strategy
-warmup(exchange, currency, warmupperiod)
+warmup(td)
 # Check bought/sold status
-balance = client.get_asset_balance(asset=asset2)
-if float(balance["free"])<10.: # this value depends for each asset - logic using USDT
-    market_status="bought"
+balance = client.get_asset_balance(asset=td.asset2)
+if float(balance["free"]) < 10.: # this value depends for each asset - logic using USDT
+    td.market_status = "bought"
 else:
-    market_status="sold"
-b_realtrade=True
+    td.market_status = "sold"
 
 # TO DO
 # HANDLE ERROR WHEN SELLING - BinanceAPIException: APIError(code=-1013): Filter failure: LOT_SIZE
